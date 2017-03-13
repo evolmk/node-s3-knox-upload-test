2770a8207286f721c78875cb0ea75c586327354e
evolmk <evolmk@gmail.com>
2017-03-13 15:22:26 -0400

    initial commit

--- knoxupload.js
+++ knoxupload.js
@@ -1,6 +1,10 @@
 'use strict';
 var knox = require('knox');
+var MultiPartUpload= require('knox-mpu');
+
 var config = require('./conf/config');
+var fs = require('fs');
+var http = require('http');
 
 var s3Client = knox.createClient({
     key: config.aws_key
@@ -11,43 +15,61 @@ var s3Client = knox.createClient({
 
 module.exports = function (server) {
 
-  // upload file
-
-  // client.putFile(filePath, newFileName, headers, function(err, res) {
-  //   if (err) return;
-  //   else if (200 == res.statusCode) {
-  //     var awsData = res.socket._httpMessage;
-  //     console.log('Uploaded to S3: ', awsData.url);
-  //   }
-  // });
-
   //setup vars & headers
-  var filePath = 'logo.png';
-  var folderPath = 'subfolder/' + filePath;
+
+  var filePath = 'logo-large.png';
+  var stream = fs.createReadStream(filePath);
+
   var headers = {
     'Content-Type': 'application/png',
     'x-amz-acl': 'public-read'
   };
+  // var headers = {
+  //   'Content-Length': res.headers['content-length'],
+  //   'Content-Type': res.headers['content-type']
+  // };
 
-  var req = s3Client.put(filePath, headers);
-  req.on('response', function(res){
-    console.log('######### Upload #########');
-    if (200 == res.statusCode) {
-      console.log('saved to %s', req.url);
-    }
+  // upload file from path
+  http.get({ host: 'google.com', path: '/' }, function (res) {
+    var headers = {
+        'Content-Length': res.headers['content-length']
+      , 'Content-Type': res.headers['content-type']
+      , 'x-amz-acl': 'public-read'
+    };
+
+    var progressHappened = false;
+
+    var req = s3Client.putStream(res, '/google', headers, function (err, res) {
+      if (200 == res.statusCode) {
+        console.log('file saved to: %s', req.url);
+      }
+      console.log(progressHappened);
+    });
+
+    req.on('progress', function (e) {
+      progressHappened = true;
+      console.log(e);
+      console.log(e.percent + '/' + e.total + ' | ' + e.written);
+    });
   });
-  req.end();
 
-  s3Client.putFile(filePath, folderPath, headers, function (err, res) {
-    console.log('######### Upload to subfolder #########');
+
+
+  // upload file from path, and set destPath (subfolder)
+  var destPath = 'subfolder/' + filePath;
+  s3Client.putFile(filePath, destPath, headers, function (err, res) {
+    console.log('\n######### Upload to subfolder #########\n');
     if (err) throw err;
-    console.log(res.statusCode);
+    if (200 == res.statusCode) {
+      console.log(filePath + ' saved to %s', destPath);
+    }
+    console.log('\nheaders:');
     console.log(res.headers);
   });
 
   // get files in bucket
   s3Client.list({}, function (err, data) {
-    console.log('######### Get All Files in Bucket #########');
+    console.log('\n######### Get All Files in Bucket #########\n');
     console.log(err, data);
   });
 
